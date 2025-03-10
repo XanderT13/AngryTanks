6abb8df4088fab73c920b808bdaa4c72798ebd38
JefVR <jef.vanriel@hotmail.com>
2025-03-10 11:58:22 +0100

    Main functions + Bug fix

--- src/AngryTanks/model/Player.java
+++ src/AngryTanks/model/Player.java
@@ -1,14 +1,18 @@
 package AngryTanks.model;
 
+import java.util.Scanner;
+
 public class Player {
     private Tank tank;
     private String name;
 
-    public Player(String name) {
+    public Player(String name, Tank tank) {
         this.name = name;
+        this.tank = tank;
     }
 
-    public void playTurn(Wind wind) {
+    public void playTurn(Wind wind, double angle, double velocity) {
+        new Trajectory(new Shot(angle, velocity), wind, tank);
     }
 
     public Tank getTank() {
--- src/AngryTanks/model/Shot.java
+++ src/AngryTanks/model/Shot.java
@@ -1,4 +1,19 @@
 package AngryTanks.model;
 
 public class Shot {
+    private double angle;
+    private double velocity;
+
+    public Shot(double angle, double velocity) {
+        this.angle = angle;
+        this.velocity = velocity;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
 }
