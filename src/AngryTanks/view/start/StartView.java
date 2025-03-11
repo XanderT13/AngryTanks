df6971d46afb2902173f5b16f552ad376c83c549
XanderT13 <XanderT13@users.noreply.github.com>
2025-03-11 16:14:13 +0100

    nieuw startscherm

--- src/AngryTanks/view/start/StartView.java
+++ src/AngryTanks/view/start/StartView.java
@@ -16,9 +16,7 @@ import javafx.scene.layout.RowConstraints;
 public class StartView extends GridPane {
     // private Node attributen (controls)
     private Button playButton;
-    private Label atLabel;
     private ImageView startSchermImage;
-    private ImageView leftTank;
     private Button settingsButton;
 
     public StartView() {
@@ -28,14 +26,9 @@ public class StartView extends GridPane {
     private void initialiseNodes() {
         playButton = new Button("PLAY");
 
-        atLabel = new Label("Angry Tanks");
-        atLabel.setStyle("-fx-font-weight: bold");
-        atLabel.setStyle("-fx-font-size: 16");
-
         startSchermImage = new ImageView();
-        leftTank = new ImageView();
 
-        settingsButton = new Button("Settings");
+        settingsButton = new Button();
     }
     private void layoutNodes() {
         // adding stylesheet
@@ -47,7 +40,20 @@ public class StartView extends GridPane {
         // achtergrond via CSS
         this.getStyleClass().add("start-view");
 
-        this.setGridLinesVisible(true);
+        settingsButton.setPrefSize(75,75);
+        settingsButton.getStyleClass().add("settings-button");
+        settingsButton.setOnMouseEntered(e -> settingsButton.setStyle(
+                "-fx-border-color: Black; " +
+                        "-fx-border-width: 2;"
+        ));
+        settingsButton.setOnMouseExited(e -> settingsButton.setStyle(
+                "-fx-border-color: lightgray; " +
+                        "-fx-border-width: 2;"
+        ));
+        this.add(settingsButton, 2, 5);
+        GridPane.setHalignment(settingsButton, HPos.CENTER);
+
+        this.setGridLinesVisible(false);
         ColumnConstraints column1 = new ColumnConstraints(200);
         ColumnConstraints column2 = new ColumnConstraints(700);
         ColumnConstraints column3 = new ColumnConstraints(200);
@@ -56,38 +62,41 @@ public class StartView extends GridPane {
         RowConstraints row2 = new RowConstraints(80);
         RowConstraints row3 = new RowConstraints(80);
         RowConstraints row4 = new RowConstraints(80);
-        RowConstraints row5 = new RowConstraints(80);
+        RowConstraints row5 = new RowConstraints(140);
         RowConstraints row6 = new RowConstraints(80);
         this.getRowConstraints().addAll(row1, row2, row3, row4, row5, row6);
 
+
+        playButton.getStyleClass().add("play-button");
+        playButton.setOnMouseEntered(e -> playButton.setStyle(
+                "-fx-background-color: darkgreen; " +  // Donkerrood bij hover
+                        "-fx-border-color: gray; " +
+                        "-fx-border-width: 2;"
+        ));
+        playButton.setOnMouseExited(e -> playButton.setStyle(
+                "-fx-background-color: green; " +  // Terug naar lichtrood
+                        "-fx-border-color: black; " +
+                        "-fx-border-width: 2;"
+        ));
         this.add(playButton, 1, 5);
         playButton.setPrefSize(120,40);
         GridPane.setHalignment(playButton, HPos.CENTER);
-
-        this.add(atLabel, 1, 0);
-        atLabel.setPrefSize(120,40);
-        GridPane.setHalignment(atLabel, HPos.CENTER);
 
         GridPane.setColumnSpan(startSchermImage, 3);
         GridPane.setRowSpan(startSchermImage, 5);
         this.add(startSchermImage, 0, 0);
 
-        this.add(leftTank, 0, 3);
-
-        this.add(settingsButton, 2, 5);
-        settingsButton.setPrefSize(100,50);
-        GridPane.setHalignment(settingsButton, HPos.CENTER);
 
     }
 
     // implementatie van de nodige
     // package-private Getters
     Button getPlayButton() {
+
         return playButton;
     }
-
-    Label getAtLabel() {
-        return atLabel;
+    Button getSettingsButton() {
+        return settingsButton;
     }
 
 }
