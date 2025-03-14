68196c67c2479fdabe7480c0e4336d4a9970bfb9
XanderT13 <XanderT13@users.noreply.github.com>
2025-03-14 12:02:30 +0100

    endscreen aangepast

--- src/AngryTanks/view/settings2/Settings2View.java
+++ src/AngryTanks/view/settings2/Settings2View.java
@@ -1,8 +1,11 @@
 package AngryTanks.view.settings2;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 
 public class Settings2View extends BorderPane {
+    private Slider volumeSlider;
 
     public Settings2View() {
         this.initialiseNodes();
@@ -10,11 +13,19 @@ public class Settings2View extends BorderPane {
     }
 
     private void initialiseNodes() {
-
+        volumeSlider = new Slider();
     }
 
     private void layoutNodes() {
+        volumeSlider.setMin(0.0);
+        volumeSlider.setMax(1.0);
+        volumeSlider.setValue(0.5);
+        volumeSlider.setShowTickLabels(true);
+        volumeSlider.setShowTickMarks(true);
+        volumeSlider.setMajorTickUnit(0.01);
+        volumeSlider.setMinorTickCount(0);
 
+        this.setCenter(volumeSlider);
     }
 }
 
